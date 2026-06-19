f068c01d3a49977c382db6ecf8fe8dfcf44a8ac3
lightlabcreation <lightlabcreation@gmail.com>
2026-06-19 19:32:32 +0530

    Update app.js
    update

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-
+// ADD THIS FOR RAILWAY
+app.set('trust proxy', 1);
 // Global Middlewares
 const allowedOrigins = [
   'http://localhost:5173',
