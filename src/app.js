ad5595294b332f36bb7c54ed464751afcbfe25d3
lightlabcreation <lightlabcreation@gmail.com>
2026-06-19 19:25:29 +0530

    Update app.js
    cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'http://localhost:3001',
+  'https://gofintaza-carrentals.netlify.app',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
